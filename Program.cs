13fbde9a0e709861b5922edf252b4a9bbe64c14d
Азиз Исматов <yourGitHub@example.co>
2020-04-19 18:31:54 +0500

    Создал 4 метода(plus,minus,division,multiply)

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+     
+        }
+        
+        
+        public static U minus<U>(U x, U y)
+        {
+           return (dynamic)x-(dynamic)y; 
+        }
+
+        public static U  plus<U>(U x,U y)
+        {
+            return (dynamic)x+(dynamic)y;   
+        }
+         public static U  division<U>(U x,U y)
+        {
+            return (dynamic)x+(dynamic)y;   
+        }
+         public static U  multiply<U>(U x,U y)
+        {
+            return (dynamic)x*(dynamic)y;   
+        }
+
+        
+  
+    }
+}
